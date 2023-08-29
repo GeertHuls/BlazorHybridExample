23133cabff7d8b5a46cdafe3ef2ab0504ddacdc2
Geert Huls <geert.huls82@gmail.com>
2023-08-29 23:02:27 +0200

    Initial setup

---
+++ BlazorHybridExample.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorHybridExample.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
