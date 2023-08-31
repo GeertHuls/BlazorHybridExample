7d2c5ea3a43d14baf4336e511d9881e439de54fa
Geert Huls <geert.huls82@gmail.com>
2023-08-31 23:46:43 +0200

    Load coffees from api

--- BlazorHybridExample.Maui/Data/CoffeeService.cs
+++ BlazorHybridExample.Maui/Data/CoffeeService.cs
@@ -1,16 +1,15 @@
-﻿namespace BlazorHybridExample.Maui.Data;
+﻿using System.Net.Http.Json;
+
+namespace BlazorHybridExample.Maui.Data;
 
 public class CoffeeService : ICoffeeService
 {
-    public async Task<IEnumerable<Coffee>> LoadCoffeesAsync()
-    {
-        var coffees = new[]
-        {
-            new  Coffee("Cappuccino", "Coffee with milk foam"),
-            new  Coffee("Doppio", "Double espresso"),
-        };
+    private readonly HttpClient _httpClient = new();
 
-        await Task.Delay(2000);
+    public async Task<IEnumerable<Coffee>?> LoadCoffeesAsync()
+    {
+        var coffees = await _httpClient.GetFromJsonAsync<IEnumerable<Coffee>>(
+            "https://www.thomasclaudiushuber.com/pluralsight/coffees.json");
 
         return coffees;
     }
