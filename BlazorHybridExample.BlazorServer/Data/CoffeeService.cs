f0432e7acec810ab5a56d7418c32d67b4d43ca2c
Geert Huls <geert.huls82@gmail.com>
2023-09-05 23:39:15 +0200

    Add blazor server project with coffees component

---
+++ BlazorHybridExample.BlazorServer/Data/CoffeeService.cs
@@ -0,0 +1,23 @@
+﻿using BlazorHybridExample.Library.Data;
+
+namespace BlazorHybridExample.BlazorServer.Data;
+
+public class CoffeeService : ICoffeeService
+{
+    private readonly IHttpClientFactory _factory;
+
+    public CoffeeService(IHttpClientFactory factory)
+    {
+        _factory = factory;
+    }
+
+    public async Task<IEnumerable<Coffee>?> LoadCoffeesAsync()
+    {
+        var httpClient = _factory.CreateClient();
+
+        var coffees = await httpClient.GetFromJsonAsync<IEnumerable<Coffee>>(
+            "https://www.thomasclaudiushuber.com/pluralsight/coffees.json");
+
+        return coffees;
+    }
+}
